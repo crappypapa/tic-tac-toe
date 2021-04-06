a6935d868fda13d06920ed296caf52eae18b4aba
crappypapa <devsolaoluwa@gmail.com>
2021-04-06 10:52:23 +0100

    Add test for cell_check method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,4 +16,49 @@ describe Player do
   end
 end
 
+describe '#cell_check' do
+  it 'return true if cell selection is between 1 and 9' do
+    cell = 4
+    arr = [1,2,3]
+    person = "ola"
+    expect(cell_check(cell,person,arr,arr)).to eql (true)
+  end
+
+  it 'return false if selection is greater than 9' do
+    cell = 11
+    arr = [1,2,3]
+    person = 'ola'
+    expect(cell_check(cell,person,arr,arr)).to eql (false)
+  end
+
+  it 'return false if selection is lesser than 1' do
+    cell = 0
+    arr = [1,2,3]
+    person = 'ola'
+    expect(cell_check(cell,person,arr,arr)).to eql (false)
+  end
+
+  it 'return false if cell selection is not an integer' do
+    cell = "str"
+    arr = [1,2,6]
+    person = 'ola'
+    expect(cell_check(cell,person,arr,arr)).to eql (false)
+  end
+
+  it "return false if cell selection has been previously taken" do
+    cell = 2
+    arr1 = [1,2,6]
+    arr2 = [3,5]
+    person = 'ola'
+    expect(cell_check(cell,person,arr1,arr2)).to eql (false)
+  end
+
+  it "return true if cell selection has not been previously taken" do
+    cell = 4
+    arr1 = [1,2,6]
+    arr2 = [3,5]
+    person = 'ola'
+    expect(cell_check(cell,person,arr1,arr2)).to eql (true)
+  end
+end
 
